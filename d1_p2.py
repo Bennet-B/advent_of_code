f0a1e7aa0a35e9d2460a807a740aa08cf591bde3
Bennet-B <106802593+Bennet-B@users.noreply.github.com>
2025-12-01 18:06:07 +0100

    solution day 1 part 2

---
+++ d1_p2.py
@@ -0,0 +1,25 @@
+INPUT_DATA: str = """L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82"""
+
+MAX_VALUE: int = 100
+START_VALUE: int = 50
+TARGET: int = 0
+
+value: int = START_VALUE
+count: int = 0
+
+for rotation in INPUT_DATA.split('\n'):
+    steps = int(rotation[1:]) * (1 if rotation[0] == 'R' else -1)
+    count += max(0, 1 + (abs(steps) - (((MAX_VALUE - value) if steps > 0 else value) % MAX_VALUE) - 1) // MAX_VALUE)
+    value = (value + steps) % MAX_VALUE
+    #print(value)
+
+print(f"\nOccurrences of {TARGET}: {count}")
