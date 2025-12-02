b995b03006a1a9f695f04953f3c55ce9950738fa
Bennet-B <106802593+Bennet-B@users.noreply.github.com>
2025-12-02 14:57:23 +0100

    2025 day 2 solutions

---
+++ 2025/day_02/d2_p1.py
@@ -0,0 +1,17 @@
+# Note: In p2, I switched to a much faster approach for performance (by generating rather than scanning all IDs); that strategy could be applied here as well with minor adjustments.
+
+INPUT_DATA: str = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
+
+sum_ids: int = 0
+
+for range_str in INPUT_DATA.split(','):
+    start, end = range_str.split('-')
+    for id in range(int(start), int(end) + 1):
+        s = str(id)
+        if len(s) % 2 == 0 and s[:len(s)//2] == s[len(s)//2:]:
+            #print(s)
+            sum += id
+
+print(f"\nSum of invalid ids: {sum_ids}")
+
+# personal solution: 19219508902
---
+++ 2025/day_02/d2_p2.py
@@ -0,0 +1,34 @@
+from typing import Iterator
+import math
+
+INPUT_DATA: str = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
+
+def divisor_pairs(n: int) -> Iterator[tuple[int, int]]:
+    for i in range(1, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            yield i, n // i
+
+def repeats(x: int, y: int) -> Iterator[int]:
+    for pattern in range(10**(x - 1), 10**x):
+        yield int(str(pattern) * y)
+    if x != 1 and y != 1 and x != y:
+        for pattern in range(10**(y - 1), 10**(y)):
+            yield int(str(pattern) * x)
+
+sum: int = 0
+
+for range_str in INPUT_DATA.split(','):
+    start, end = range_str.split('-')
+    lower, upper = max(11, int(start)), int(end)
+    for length in range(len(start), len(end) + 1):
+        checked = set()
+        for x, y in divisor_pairs(length):
+            for invalidid in repeats(x, y):
+                if lower <= invalidid <= upper and invalidid not in checked:
+                    print(invalidid)
+                    sum += invalidid
+                    checked.add(invalidid)
+
+print(f"\nSum of invalid ids: {sum}")
+
+# personal solution: 27180728081
